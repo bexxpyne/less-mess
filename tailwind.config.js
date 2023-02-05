cd592066acdebbcd3ed8beb5bde35f224b211770
bexxpyne <bexx@mac.com>
2023-02-05 16:41:11 +0000

    got the rav thing working (ish)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./styles/**/*.{html,js}",
+    "./index.html",
+    "./craft.html",
+    "./contact.html",
+    "./saltnames.html"
+    
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
